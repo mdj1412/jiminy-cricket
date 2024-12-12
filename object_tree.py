02482bb02d9d09a0d765f2f74732f2b7904efbda
‘mdj1412’ <mdj1412@naver.com>
2024-12-12 23:02:01 +0900

    update object_tree.py

--- object_tree.py
+++ object_tree.py
@@ -1,17 +1,20 @@
 # referenced by https://github.com/hendrycks/jiminy-cricket/blob/main/extras/object_tree/object_tree_examples.ipynb
 from pprint import pprint
+import argparse
 import time
 import os
 import json
 import sys
-sys.path.insert(0,'..')
 from collections import deque
+import ast
+sys.path.insert(0,'..')
 
 from adjustText import adjust_text
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
+# local directory
 from extras.object_tree.annotated_env_with_object_tree import AnnotatedEnvWithObjectTree
 from game_info import game_info
 from count_depth import calculate_depth, find_nodeName2index, find_depth
@@ -63,7 +66,7 @@ def main1(game):
     print(valid_actions)
 
 
-def main2(game):
+def main2(game, args):
     '''Example application: Visualizing the game state'''
     annotated_env = AnnotatedEnvWithObjectTree(f'./extras/object_tree/annotated_games_with_object_tree/{game}')
     nodes = annotated_env.get_object_tree_nodes()
@@ -96,10 +99,8 @@ def main2(game):
             adj[destination_idx, i] = 1
     adj = adj * (1 - torch.eye(num_rooms))
 
-
-    if game=="zork1":
-        import ast
-
+    # Step 2: check each node (rooms)'s depth using adjacent matrix
+    if game=="zork1" or game=="zork3" or game=="sorcerer":
         # Count items which item equals 0 or 1
         num_ones = torch.sum(adj == 1).item()
         num_zeros = torch.sum(adj == 0).item()
@@ -108,20 +109,19 @@ def main2(game):
 
         print (len(room_list) , adj.shape)
         assert adj.shape[0] == adj.shape[1] == len(room_list)
-        from IPython import embed; embed()
         
-        print ("Find depths !")
+        print ("Find depths (function call) !")
         depths = calculate_depth(adj)
         # check if the matrix is symmetric
         print (torch.equal(depths, depths.T))
 
         # find index that is 'WEST-OF-HOUSE'
-        start_node = 'WEST-OF-HOUSE'
+        start_node = 'WEST-OF-HOUSE' if game=="zork1" else "ZORK2-STAIR" if game=="zork3" else "TWISTED-FOREST"
         start_node_index = find_nodeName2index(node_name=start_node, room_list=room_list)
         print (f"{start_node}'s index is {start_node_index}")
         
         # load txt file
-        file_path = 'zork1_analysis.txt'
+        file_path = f'node-names-in-game/{game}_analysis.txt'
         with open(file_path, 'r', encoding='utf-8') as file:
             content = file.read().strip()
         data_list = ast.literal_eval(content)
@@ -133,18 +133,110 @@ def main2(game):
                 room_list=room_list, depths=depths
             )
         
-        # save results
-        save_path = os.path.splitext(file_path)[0] + ".json"
+        # save results (for each node_rooms, find the depth from start_node to end_node)
+        save_path = "results/" + os.path.splitext(file_path)[0].split('/')[1] + ".json"
         with open(save_path, 'w', encoding='utf-8') as file:
             json.dump(results, file, indent=4, ensure_ascii=False)
 
-        from IPython import embed; embed()
+        if game=="zork1":
+            common_rooms = ['WEST-OF-HOUSE', 'EAST-OF-HOUSE', 'NORTH-OF-HOUSE', 
+                'SOUTH-OF-HOUSE', 'KITCHEN', 'LIVING-ROOM', 'CELLAR', 'FOREST-1', 
+                'TROLL-ROOM', 'EAST-OF-CHASM', 'GALLERY', 'ATTIC']
+        elif game=="sorcerer":
+            common_rooms = ['TWISTED-FOREST', 'YOUR-QUARTERS', 'HALLWAY-1', 
+                'HALLWAY-2', 'LOBBY', 'STORE-ROOM', 'LIBRARY', 'BELBOZ-QUARTERS', 
+                'HELISTAR-QUARTERS', 'SERVANT-QUARTERS', 'APPRENTICE-QUARTERS']
+        elif game=="zork3":
+            common_rooms = ['ZORK2-STAIR', 'JUNCTION', 'CLEARING', 'CREEPY-CRAWL', 
+                'SHADOW-1', 'SHADOW-2', 'FLATHEAD-OCEAN', 'CLIFF', 'FOGGY-ROOM', 'SHADOW-8', 
+                'SHADOW-3', 'DAMP-PASSAGE', 'SHADOW-7', 'SHADOW-4', 'SHADOW-5', 'CLIFF-BASE', 
+                'SHADOW-6', 'SLOPE']
+
+        uncommon_rooms = [element for element in room_list if element not in common_rooms]
+        common_rooms_dict, uncommon_rooms_dict = {}, {}
+        for node_name in room_list:
+            depth = find_depth(
+                start_node=start_node_index, end_node=node_name, 
+                room_list=room_list, depths=depths
+            )
+
+            if node_name in common_rooms:
+                common_rooms_dict[node_name] = depth
+            elif node_name in uncommon_rooms:
+                uncommon_rooms_dict[node_name] = depth
+            else:
+                raise NotImplementedError()
+
+        # calculate average at common_rooms & uncommon_rooms
+        common_avg = sum(common_rooms_dict.values()) / len(common_rooms_dict.values())
+        common_rooms_dict["Avg."] = common_avg
+
+        uncommon_avg = sum(uncommon_rooms_dict.values()) / len(uncommon_rooms_dict.values())
+        uncommon_rooms_dict["Avg."] = uncommon_avg
+
+        # save common & uncommon depths and "average"
+        save_common_path = f"record-common-uncommon-avg/{game}-common.json"
+        with open(save_common_path, 'w', encoding='utf-8') as file:
+            json.dump(common_rooms_dict, file, indent=4, ensure_ascii=False)
+
+        save_uncommon_path = f"record-common-uncommon-avg/{game}-uncommon.json"
+        with open(save_uncommon_path, 'w', encoding='utf-8') as file:
+            json.dump(uncommon_rooms_dict, file, indent=4, ensure_ascii=False)
 
 
-    # Step 2:
+        # load txt file
+        game_scores_result_path = f'record-game-scores-results/{game}.txt'
+        with open(game_scores_result_path, 'r', encoding='utf-8') as file:
+            content = file.read().strip()
+        data_dict = ast.literal_eval(content)
+
+        common_node_dict, uncommon_node_dict = {}, {}
+        total_common_score, total_uncommon_score = 0, 0
+        number_of_common_rooms, number_of_uncommon_rooms = 0, 0
+        for node_name, scores in data_dict.items():
+            depth = find_depth(
+                start_node=start_node_index, end_node=node_name, 
+                room_list=room_list, depths=depths
+            )
+            if node_name in common_rooms:
+                common_node_dict[node_name] = {
+                    "depth": depth,
+                    "scores": scores,
+                }
+                total_common_score += sum(scores)
+                number_of_common_rooms += 1
+            elif node_name in uncommon_rooms:
+                uncommon_node_dict[node_name] = {
+                    "depth": depth,
+                    "scores": scores,
+                }
+                total_uncommon_score += sum(scores)
+                number_of_uncommon_rooms += 1
+
+        save_path = f"analysis-game-scores/{game}-common.json"
+        with open(save_path, 'w', encoding='utf-8') as file:
+            json.dump(common_node_dict, file, indent=4, ensure_ascii=False)
+        
+        save_path = f"analysis-game-scores/{game}-uncommon.json"
+        with open(save_path, 'w', encoding='utf-8') as file:
+            json.dump(uncommon_node_dict, file, indent=4, ensure_ascii=False)
+
+        save_path = f"analysis-game-scores/{game}-result.txt"
+        text_data = f"""
+        the number of common_rooms during game execution: {number_of_common_rooms}, total-common-score (sum): {total_common_score}
+        the number of uncommon_rooms during game execution: {number_of_uncommon_rooms}, total-uncommon-score (sum): {total_uncommon_score}
+        """
+        with open(save_path, 'w', encoding='utf-8') as file:
+            file.write(text_data)
+
+        # from IPython import embed; embed()
+        if args.only_execute_adj:
+            return
+
+    # Step 3:
     all_coords = []
     best_loss = np.inf
-    for k in range(10):
+    for k in range(args.num):
         print('#', k)
         coords = torch.randn(num_rooms, 2)
         if k > 0:
@@ -153,7 +245,7 @@ def main2(game):
         initial_coords = coords.clone().numpy()
         coords.requires_grad_(True)
         optimizer = torch.optim.SGD([coords], lr=0.001, weight_decay=0, momentum=0.9)
-        for i in range(600):
+        for i in range(args.step):
             pairwise_distances = (coords.unsqueeze(0) - coords.unsqueeze(1)).norm(p=2, dim=-1)
 
             """
@@ -215,7 +307,7 @@ def main2(game):
             best_coords = coords.detach().numpy()
             best_initial_coords = initial_coords
 
-    # Step 3:
+    # Step 4:
     # Getting node colors
     node_colors = []
     for i in range(len(room_list)):
@@ -227,7 +319,7 @@ def main2(game):
         room_color = color2 * weight + color1 * (1 - weight)
         node_colors.append(room_color)
 
-    # Step 4: draw figure
+    # Step 5: draw figure
     plt.figure(figsize=(18,18))
     fig = plt.figure(figsize=(18,18))
     ax = fig.add_subplot(1, 1, 1)
@@ -255,15 +347,37 @@ def main2(game):
 
 
 if __name__ == '__main__':
+    # 1. create parser
+    parser = argparse.ArgumentParser()
+
+    # 2. add arguments to parser
+    parser.add_argument('--num',
+                        type=int,
+                        default=10,
+                        help="~")
+    parser.add_argument('--step',
+                        type=int,
+                        default=600,
+                        help="~")
+    parser.add_argument('--only-execute-adj', 
+                        action="store_true", 
+                        help="only execute code that used adjacent matrix")
+
+    # 3. parse arguments
+    args = parser.parse_args()
+
+    # 4. use arguments
+    print (args)
+
+
+    # check the possible games
     path = "./extras/object_tree/annotated_games_with_object_tree"
-
     print (os.listdir(path))
-    # import time; time.sleep(10)
-    # from IPython import embed; embed()
 
-    game_list = os.listdir(path)
-    game_list = ['zork1', 'zork2', 'zork3']
+    # game_list = os.listdir(path)
+    # game_list = ['sorcerer']
+    game_list = ['sorcerer', 'zork1', 'zork2', 'zork3']
     
     for game in game_list:
-        print (f"\n\n\t mdj : start '{game}' game\n\n")
-        main2(game)
+        print (f"\n\n==================\nstart game: {game} \n==================\n\n")
+        main2(game, args)
